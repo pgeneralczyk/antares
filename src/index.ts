f2905e4403f2af70653b65323b103a62263fd8e5
Pawel Generalczyk <pawelgeneralczyk@Pawels-MacBook-Air.local>
2026-02-23 22:37:04 +0100

    Configure base routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+app.get("/rental/recurring-rates", () => Bun.file("./mocks/get-rental-recurring-rates.json").json());
+app.get("/rental/contract/100549", () => Bun.file("./mocks/get-rental-contracts-100549.json").json());
+app.get("/rental/contract/100549/invoice-periods", () => Bun.file("./mocks/get-rental-contracts-100549-invoice-periods.json").json());
+app.get("/rental/contract/100549/items", () => Bun.file("./mocks/get-rental-contracts-100549-items.json").json());
+app.get("/rental/contract/100549/items/14/rental-units", () => Bun.file("./mocks/get-rental-contracts-100549-items-14-rental-units.json").json());
+
+app.listen(3000);
