6bfe3c4d57360a68809991ce78dc94418d60fcf6
Paweł Generalczyk <pawel.generalczyk@outlook.com>
2026-04-08 20:28:44 +0200

    Improvements of query response

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 
-const app = new Elysia();
+const app = new Elysia()
+  .use(
+    cors({
+      origin: "http://127.0.0.1:4321",
+      methods: ["GET", "POST", "OPTIONS"],
+      allowedHeaders: ["Content-Type"],
+    })
+  );
 
 app.post("/query", () => Bun.file("./mocks/post-query-rct-100639.json").json());
 app.get("/rental/recurring-rates", () => Bun.file("./mocks/get-rental-recurring-rates.json").json());
-app.get("/rental/contract/100549", () => Bun.file("./mocks/get-rental-contracts-100549.json").json());
-app.get("/rental/contract/100549/invoice-periods", () => Bun.file("./mocks/get-rental-contracts-100549-invoice-periods.json").json());
-app.get("/rental/contract/100549/items", () => Bun.file("./mocks/get-rental-contracts-100549-items.json").json());
-app.get("/rental/contract/100549/items/14/rental-units", () => Bun.file("./mocks/get-rental-contracts-100549-items-14-rental-units.json").json());
+app.get("/rental/contracts/100549", () => Bun.file("./mocks/get-rental-contracts-100549.json").json());
+app.get("/rental/contracts/100639", () => Bun.file("./mocks/get-rental-contracts-100639.json").json());
+app.get("/rental/contracts/100549/invoice-periods", () => Bun.file("./mocks/get-rental-contracts-100549-invoice-periods.json").json());
+app.get("/rental/contracts/100549/items", () => Bun.file("./mocks/get-rental-contracts-100549-items.json").json());
+app.get("/rental/contracts/100549/items/14/rental-units", () => Bun.file("./mocks/get-rental-contracts-100549-items-14-rental-units.json").json());
 
-app.listen(3000);
+app.listen(8889);
